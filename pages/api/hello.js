39cb7e990793947cf1439329e98c597e4c1592a1
Zac Colley <zaccolley@fastmail.com>
2021-09-02 14:04:12 +0100

    Test database connection

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,21 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import faunadb from 'faunadb';
+
+const { FAUNA_ADMIN_KEY } = process.env;
+ 
+const q = faunadb.query;
+const client = new faunadb.Client({ secret: FAUNA_ADMIN_KEY });
+
+var createP = client.query(
+  q.Create(
+    q.Collection('test'),
+    { data: { testField: 'testValue' } }
+  )
+)
 
 export default function handler(req, res) {
+  createP.then(function(response) {
+  console.log(response.ref); // Logs the ref to the console.
+})
+  
   res.status(200).json({ name: 'John Doe' })
 }
